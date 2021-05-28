9dfc4eacd8f77784d362c72862735ed74e531890
Ross Ragsdale <ross.ragsdale@gmail.com>
2021-05-28 11:10:38 -0500

    :zap: Clean up

--- src/App.js
+++ src/App.js
@@ -1,53 +1,54 @@
-import { request, gql } from 'graphql-request';
-import React, { useEffect, useState } from 'react';
-import { useMutation } from 'react-query';
-import { StreamChat } from 'stream-chat';
-import { Chat, Channel, ChannelHeader, ChannelList, LoadingIndicator, MessageInput, MessageList, Thread, Window } from 'stream-chat-react';
+import { request, gql } from "graphql-request";
+import React, { useEffect, useState } from "react";
+import { useMutation } from "react-query";
+import { StreamChat } from "stream-chat";
+import {
+  Chat,
+  Channel,
+  ChannelHeader,
+  ChannelList,
+  LoadingIndicator,
+  MessageInput,
+  MessageList,
+  Thread,
+  Window,
+} from "stream-chat-react";
 
-import 'stream-chat-react/dist/css/index.css';
-
-
-function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
-}
-
-const id = getRandomInt(1000).toString()
+import "stream-chat-react/dist/css/index.css";
 
 const createToken = gql`
-  mutation {
-    createPost(id: ${id}) {
-      token
-    }
+  mutation CreateToken($name: String!) {
+    createToken(name: $name)
   }
-`
+`;
 
+const name = "Ross";
 
-
-const filters = { type: 'messaging', members: { $in: [id] } };
+const filters = { type: "messaging", members: { $in: [name] } };
 const sort = { last_message_at: -1 };
-
-
 
 const App = () => {
   const [chatClient, setChatClient] = useState(null);
-  
+
   // const mutation = useMutation(() => request('https://sles5aebb2.execute-api.af-south-1.amazonaws.com/dev/graphql', createToken))
-  const mutation = useMutation(() => request('http://localhost:4000', createToken))
+  const mutation = useMutation(() => request("http://localhost:4000", createToken, { name: name }));
   useEffect(() => {
     const initChat = async () => {
-      const client = StreamChat.getInstance('92ev7u4spvvh');
-      const {createPost} = await mutation.mutateAsync()
-      console.log(createPost)
-      await client.connectUser(
-        {
-          id: id,
-          name: id,
-          image: 'https://getstream.io/random_png/?id=odd-night-9&name=odd-night-9',
-        },
-        createPost.token
-      );
+      const client = StreamChat.getInstance("92ev7u4spvvh");
+      const { createToken } = await mutation.mutateAsync();
 
-      setChatClient(client);
+      if (createToken) {
+        await client.connectUser(
+          {
+            id: name,
+            name: name,
+            image: "https://getstream.io/random_png/?id=odd-night-9&name=odd-night-9",
+          },
+          createToken
+        );
+
+        setChatClient(client);
+      }
     };
 
     initChat();
@@ -58,7 +59,7 @@ const App = () => {
   }
 
   return (
-    <Chat client={chatClient} theme='messaging dark'>
+    <Chat client={chatClient} theme="messaging dark">
       <ChannelList filters={filters} sort={sort} />
       <Channel>
         <Window>
